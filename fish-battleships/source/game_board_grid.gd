5ece5b3bd5ccc7b4d220a41ebc27880efd291778
Wana <wana.qi.33425@gmail.com>
2025-09-29 20:57:04 -0500

    minor cleanups

--- fish-battleships/source/game_board_grid.gd
+++ fish-battleships/source/game_board_grid.gd
@@ -1,23 +1,8 @@
 class_name GameBoardGrid extends Node2D
 
-"""
-State:
-	some kind of grid
-		each cell can hold a pointer to a fish maybe?
-	also know all the fish and their positions
-	Probably a fish would be child of the board
-
-Methods:
-	register a fish and position (we trigger separately) + other CRUD methods in future
-	
-"""
-
 const GRID_SIZE := Vector2(128, 128)
 
 var grid: Array = []
-
-func is_legal_tile(tile_coord: Vector2i):
-	return tile_coord.x >= 0 and tile_coord.x < 8 and tile_coord.y >= 0 and tile_coord.y < 8
 
 func _ready():
 	var top_left_tile_center := position + (GRID_SIZE / 2)
@@ -37,16 +22,14 @@ func _draw():
 func _process(delta):
 	queue_redraw()
 
-func handle_fish_placement(fish: Fish, pos: Vector2):
-	print("Tried place fish at ", pos)
-	var position_as_tile_coord: Vector2i = $TileMapLayer.local_to_map(to_local(pos))
-	# find out which tile is pos
-	print("Tile pos ", position_as_tile_coord)
-	# if it's not in the grid, we're doomed haha
-	for fish_shape_relative_coordinate in fish.get_shape_as_tile_offsets():
-		var tile_to_check = position_as_tile_coord + fish_shape_relative_coordinate	
-		if not is_legal_tile(tile_to_check):
-			print("Failed, illegal psoition")
+func handle_fish_placement(fish: Fish, placed_position_global: Vector2) -> void:
+	var placement_pos_as_tile_coordinate: Vector2i = $TileMapLayer.local_to_map(to_local(placed_position_global))
+	for fish_shape_offset in fish.get_shape_as_tile_offsets():
+		var fish_body_coordinate_to_check: Vector2i = placement_pos_as_tile_coordinate + fish_shape_offset	
+		if not is_legal_tile(fish_body_coordinate_to_check):
+			print("Failed, illegal position")
 			return
-	
 	print("good position!")
+
+func is_legal_tile(tile_coord: Vector2i):
+	return tile_coord.x >= 0 and tile_coord.x < 8 and tile_coord.y >= 0 and tile_coord.y < 8
