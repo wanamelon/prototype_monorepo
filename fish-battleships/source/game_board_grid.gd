5f22d0db50d9e8927f17801b0368b5bfd34ea66e
Wana <wana.qi.33425@gmail.com>
2025-10-08 22:33:47 -0500

    Place items on drop

--- fish-battleships/source/game_board_grid.gd
+++ fish-battleships/source/game_board_grid.gd
@@ -1,6 +1,7 @@
 class_name GameBoardGrid extends Node2D
 
 const GRID_SIZE := Vector2(128, 128)
+const PLACED_ITEM_SCENE: PackedScene = preload("res://source/placed_item.tscn")
 
 @onready
 var grid: Array[Vector2i] = $TileMapLayer.get_used_cells()
@@ -29,7 +30,15 @@ func _can_drop_data(local_position: Vector2, dragged_item_data: Variant):
 
 func _drop_data(local_position: Vector2, dragged_item_data: Variant):
 	print("Dropped ", dragged_item_data, " at ", local_position)
-	pass
+	var item_data := dragged_item_data as TestPreview
+	var placed_item = PLACED_ITEM_SCENE.instantiate()
+	placed_item.rotation = item_data.rotation
+	# Figure out where item should snap to
+	var fish_shape_offset = item_data.get_shape_as_offsets()[0]
+	var tile_for_item_segment: Vector2i = $TileMapLayer.local_to_map(local_position + fish_shape_offset)
+	var local_position_for_tile: Vector2 = $TileMapLayer.map_to_local(tile_for_item_segment)
+	placed_item.position = (local_position_for_tile - fish_shape_offset).snapped(Vector2.ONE)
+	add_child(placed_item)
 
 func is_legal_tile(tile_coord: Vector2i) -> bool:
 	return tile_coord in grid
