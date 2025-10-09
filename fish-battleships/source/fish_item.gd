9d692caa0a0681496f02ae8d2af660b6a2d4929a
Wana <wana.qi.33425@gmail.com>
2025-10-08 22:16:28 -0500

    Hide original item while being dragged
    We need to nest the item under a parent control, reason being
    the preview node will be added to the highest ancestor control
    in the tree. If we don't, then hiding the item will hide the
    preview which is bad!

--- fish-battleships/source/fish_item.gd
+++ fish-battleships/source/fish_item.gd
@@ -2,10 +2,19 @@ extends Control
 
 var preview: PackedScene = preload("res://source/test_preview.tscn")
 
+func _notification(what):
+	match what:
+		NOTIFICATION_DRAG_END:
+			if is_drag_successful():
+				queue_free()
+			else:
+				show()
+
 func _get_drag_data(at_position: Vector2) -> Control:
 	#var drag_data := DragData.new()
 	var new_preview: Control = preview.instantiate()
 	#drag_data.rotated.connect(func (rotation_rads: float): new_preview.rotation = rotation_rads)
 	set_drag_preview(new_preview)
 	#add_child(drag_data)
+	hide()
 	return new_preview
