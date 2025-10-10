2887594120a89d1ab1320d8b9e89b77b61212bab
Wana <wana.qi.33425@gmail.com>
2025-10-09 21:12:14 -0500

    Check drag drop legality on rotate
    Otherwise, if we don't move mouse but do rotate the preview,
    Godot won't call _can_drop_data, and we won't
    update highlighting in grid

--- fish-battleships/source/test_preview.gd
+++ fish-battleships/source/test_preview.gd
@@ -6,9 +6,11 @@ func _input(event):
 	if event.is_action_pressed("RotateClockwise"):
 		rotation = snapped(rotation + QUARTER_TURN, QUARTER_TURN)
 		get_viewport().set_input_as_handled() # Otherwise, right click cancels drag
+		get_viewport().update_mouse_cursor_state() # Ensure we call _can_drop_data even if mouse stationary
 	elif event.is_action_pressed("RotateCounterClockwise"):
 		rotation = snapped(rotation - QUARTER_TURN, QUARTER_TURN)
 		get_viewport().set_input_as_handled()
+		get_viewport().update_mouse_cursor_state()
 
 func get_shape_as_offsets() -> Array[Vector2]:
 	var rotated: Array[Vector2] = []
@@ -16,3 +18,4 @@ func get_shape_as_offsets() -> Array[Vector2]:
 		var raw_position = child.position.rotated(rotation)
 		rotated.append(raw_position.snapped(Vector2.ONE))
 	return rotated
+
