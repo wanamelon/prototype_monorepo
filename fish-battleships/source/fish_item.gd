0f162061ca29c2c68350b8b533a96009412c984b
Wana <wana.qi.33425@gmail.com>
2025-10-11 13:50:12 -0500

    remove unused code

--- fish-battleships/source/fish_item.gd
+++ fish-battleships/source/fish_item.gd
@@ -11,10 +11,7 @@ func _notification(what):
 				show()
 
 func _get_drag_data(at_position: Vector2) -> Control:
-	#var drag_data := DragData.new()
 	var new_preview: Control = preview.instantiate()
-	#drag_data.rotated.connect(func (rotation_rads: float): new_preview.rotation = rotation_rads)
 	set_drag_preview(new_preview)
-	#add_child(drag_data)
 	hide()
 	return new_preview
