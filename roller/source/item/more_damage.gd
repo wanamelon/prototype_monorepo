2093b0543bcf8770678e04a5b639b0f59f08b1b2
Wana <wana.qi.33425@gmail.com>
2025-11-02 16:28:20 -0600

    Get rid of needless "bit over 1 delta" status effect

--- roller/source/item/more_damage.gd
+++ roller/source/item/more_damage.gd
@@ -4,4 +4,4 @@ func activate(state: MatchState):
 	for item in state.items:
 		if item is Roller:
 			_add_event(ItemSystem.AddStatusEffect.new(
-				ItemSystem.StatusEffect.new(ItemSystem.StatusEffectId.DAMAGE_BUFF, 1, state.delta * 1.1), item))
+				ItemSystem.StatusEffect.new(ItemSystem.StatusEffectId.DAMAGE_BUFF, 1, 0), item))
