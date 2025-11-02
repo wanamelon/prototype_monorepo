eefc57ec8ccd766cac8936b98a0f27662f38fbf5
Wana <wana.qi.33425@gmail.com>
2025-11-02 16:26:07 -0600

    minor simplify

--- roller/source/item/add_stamina.gd
+++ roller/source/item/add_stamina.gd
@@ -3,8 +3,7 @@ class_name AddStamina extends ItemSystem.Item
 var _items_added_stamina_for = []
 
 func activate(state: MatchState):
-	for item in state.items:
-		if item is Roller and not item in _items_added_stamina_for:
-			_add_event(ItemSystem.AddStatusEffect.new(
-				ItemSystem.StatusEffect.new(ItemSystem.StatusEffectId.STAMINA_BUFF, 2, 1e9), item))
-			_items_added_stamina_for.append(item)
+	for roller: Roller in Utils.filter(state.items, func(i): return i is Roller and not i in _items_added_stamina_for):
+		_add_event(ItemSystem.AddStatusEffect.new(
+			ItemSystem.StatusEffect.new(ItemSystem.StatusEffectId.STAMINA_BUFF, 2, 1e9), roller))
+		_items_added_stamina_for.append(roller)
